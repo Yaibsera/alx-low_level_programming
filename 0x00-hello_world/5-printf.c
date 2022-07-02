a9733d934292381c2e0f62b79452aa6e51da1cfb
yabsera <fyaibsera@gmail.com>
2022-07-02 00:01:01 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /** 
- * * Description: main -print out a string stdout.
+ * * Description: main - print out a string stdout.
  *
- * * Return: 0 success.
- */
+ * * Return: always 0(success) 
+*/
+
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
