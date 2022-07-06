0e6dc9b128823bc967a2e0dd67046bc45a828402
yabsera <fyaibsera@gmail.com>
2022-07-06 04:44:55 -0700

    fix this problem

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -4,8 +4,8 @@
  * -putchar - writes the character c to stdout @c: the character to print 
  *  return: on success 1.
  *  on error, -1 is returned, and error is set appropriately.
- */
-int -putchar(char c)
+*/
+int _putchar(char c)
 
 {
    return (write(1, &c, 1));
